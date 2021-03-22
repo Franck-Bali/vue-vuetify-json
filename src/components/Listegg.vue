57e77062505908588ce7a2acf6846b61264bd757
Franck-Bali <72269267+Franck-Bali@users.noreply.github.com>
2021-03-22 17:50:25 +0300

    Update Listegg.vue

--- src/components/Listegg.vue
+++ src/components/Listegg.vue
@@ -1,8 +1,37 @@
 <template>
     <div>
         <h1>{{this.titre}}</h1>
+        <u>
+            <h2 class="mt-4">
+                Description :
+            </h2>
+        </u>
         <p>{{this.description}}</p>
-        <h4>{{this.prix}}</h4>
+
+        <u>
+            <h2 class="mt-4">
+                Prix :
+            </h2>
+        </u>
+        <h4>{{this.prix}} $</h4>
+        <!--v-btn v-show="nombre > 1" @click="nombre--">-</v-btn-->
+        <div class="mt-4">
+            <u>
+                <h2 class="mt-4">
+                    Quantités :
+                </h2>
+            </u>
+            <v-btn :disabled="nombre > 1 ? false : true" @click="nombre--">
+                -
+            </v-btn>
+            <input type="number" v-model="nombre" min="1" max="10"/>
+            <v-btn :disabled="nombre < 10 ? false : true" @click="nombre++">
+                +
+            </v-btn>
+        </div>
+        <div class="mt-4" style="text-align:center;">
+            <v-btn block color="red accent-3" dark @click="achat(titre,nombre,prix)">Acheter</v-btn>
+        </div>
     </div>
 </template>
 <script>
@@ -13,6 +42,16 @@ export default {
         description: String,
         prix: Number
     },
+    data(){
+        return{
+            nombre: 1,
+        }
+    },
+    methods:{
+        achat: function(t,n,p){
+            return alert("achat de "+t+" nombre: "+n+", pour : "+p+" . Total : "+(p*n)+"$");
+        }
+    }
 }
 </script>
 <style scoped>
